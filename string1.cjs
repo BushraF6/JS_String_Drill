e6596daf12f176802be34786747b67c8747499d6
BushraFatima <138645454+BushraF6@users.noreply.github.com>
2023-08-14 11:18:44 +0530

    Get equivalent numeric values from price strings.

---
+++ string1.cjs
@@ -0,0 +1,20 @@
+function convertToNumber(strList) {
+
+    for (str = 0; str < strList.length; str++) {
+
+        strList[str] = strList[str].replace('$', '');
+        strList[str] = strList[str].replace(',', '');
+        strList[str] = Number(strList[str]);
+
+        if (!isNaN(strList[str])) {
+            continue;
+        } else{
+            strList[str] = 0;
+        }
+    }
+
+    return strList;
+}
+
+module.exports = convertToNumber;
+

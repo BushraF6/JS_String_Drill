46704223b259739448bf1b97108b7c9c190395f8
BushraFatima <138645454+BushraF6@users.noreply.github.com>
2023-08-14 15:19:55 +0530

    Added test file for string problem 6.

---
+++ stringtest/testString6.cjs
@@ -0,0 +1,8 @@
+// Importing modules.
+let string6 = require('../string6.cjs');
+// Given Data.
+let camelCase = 'thisIsATest';
+// Converting camel case to snake case..
+const snakeCase = string6(camelCase);
+// Printing converted string.
+console.log(snakeCase);

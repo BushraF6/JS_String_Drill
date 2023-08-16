053191e19de042b6be779b9e77f3b875fc1f0ba8
BushraFatima <138645454+BushraF6@users.noreply.github.com>
2023-08-16 11:53:05 +0530

    Updated code to handle errors.

--- string4.cjs
+++ string4.cjs
@@ -1,13 +1,20 @@
 function getName (nameObj) {
-    let names = Object.values(nameObj);
 
-    for (let char = 0; char < names.length; char++) {
-        
-        names[char] = names[char].toLowerCase();
-        names[char] = names[char].replace(names[char][0], names[char][0].toUpperCase());
+    if (typeof nameObj === 'object' && nameObj !== {}) {
+        let names = Object.values(nameObj);
+
+        for (char = 0; char < names.length; char++) {
+            
+            names[char] = names[char].toLowerCase();
+            names[char] = names[char].replace(names[char][0], names[char][0].toUpperCase());
+        }
+
+        return names = names.join(' ');
+
+    } else {
+        return '';
     }
-
-    return names = names.join(' ');
+    
 }
 
 module.exports = getName;

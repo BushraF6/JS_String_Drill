61e74e0dbffa538797480782a4bd4c11333c0b6b
BushraFatima <138645454+BushraF6@users.noreply.github.com>
2023-08-16 11:51:37 +0530

    Updated code to handle errors.

--- string3.cjs
+++ string3.cjs
@@ -1,9 +1,25 @@
-function getMonth (date) {
+function getMonth(date) {
 
-    let dateList = date.split('/');
-    return dateList[1];
+    if (typeof date === 'string' && date && date.includes('/')){
+        let dateList = date.split('/');
+        let month;
+
+        for (let num = 0; num < dateList.length; num++){     
+            dateList[num] = Number(dateList[num]);
+
+            if (!isNaN(dateList[num])) {
+                month = dateList[1];
+            } else {
+                month = '';
+            }
+        }
+
+        return month;
+
+    } else { 
+        return '';
+    }
     
 }
 
 module.exports = getMonth;
-

c2b6da7bed0d4ca16a64fa431c2d878fff95d00e
BushraFatima <138645454+BushraF6@users.noreply.github.com>
2023-08-16 11:49:59 +0530

    Updated code to handle errors.

--- string1.cjs
+++ string1.cjs
@@ -1,19 +1,31 @@
 function convertToNumber(strList) {
 
-    for (let str = 0; str < strList.length; str++) {
+    if (Array.isArray(strList) && strList.length > 0) {
 
-        strList[str] = strList[str].replace('$', '');
-        strList[str] = strList[str].replace(',', '');
-        strList[str] = Number(strList[str]);
+        for (str = 0; str < strList.length; str++) {
 
-        if (!isNaN(strList[str])) {
-            continue;
-        } else{
-            strList[str] = 0;
+            if ( typeof strList[str] === 'string' && strList[str] ) {
+
+                strList[str] = strList[str].replace('$', '');
+                strList[str] = strList[str].replace(',', '');
+                strList[str] = Number(strList[str]);
+        
+                if (!isNaN(strList[str])) {
+                    continue;
+                }else {
+                    strList[str] = 0;
+                }
+
+            }else {
+                strList = [];
+            }  
         }
-    }
+        return strList;
 
-    return strList;
+    }else {
+        return [];
+    }
+    
 }
 
 module.exports = convertToNumber;

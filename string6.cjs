1124e521cde531c74a09d8e4f29ea2730c897774
BushraFatima <138645454+BushraF6@users.noreply.github.com>
2023-08-16 11:55:10 +0530

    Updated code to handle errors.

--- string6.cjs
+++ string6.cjs
@@ -1,16 +1,21 @@
 function getSnakeCase (string) {
-    let newString = string;
+    if (typeof string === 'string' && string) {
+        let newString = string;
 
-    for (let i = 0; i < string.length; i++) {
+        for (let i = 0; i < string.length; i++) {
 
-        if (string[i] === string[i].toUpperCase()) {
-            newString = newString.replace(string[i], '_' + string[i]);
+            if (string[i] === string[i].toUpperCase()) {
+                newString = newString.replace(string[i], '_' + string[i]);
+            }
         }
-    }
 
-    newString = newString.replace(string[0], string[0].toUpperCase());
-    return newString;
+        newString = newString.replace(string[0], string[0].toUpperCase());
+        return newString;
+
+    } else {
+        return '';
+    }
+    
 }
 
 module.exports = getSnakeCase;
-
